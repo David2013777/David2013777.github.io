202fde26b199b93ae3797be5e415c8cd13bf98b3
张立豪 <2019819305@qq.com>
2022-02-27 14:12:24 +0800

    Site updated: 2022-02-27 14:12:22

--- js/welcome.js
+++ js/welcome.js
@@ -1,15 +1,15 @@
 function welcome(){
-    let welcome_text = '欢迎光顾本蒟蒻的小窝~'
+    let welcome_text = '欢迎来到我的网站！'
     if(document.referrer!==''){
         let referrer=document.referrer.split("/")[2];
         welcome_text="欢迎你，来自"+referrer.toUpperCase()+"的用户！";//获取用户来源域名
     }
     swal({
-        title: " 欢迎！",
-        text: welcome_text+'\n打开页面下方音乐以获得更佳体验！',//欢迎文本，可自行修改
-        imageUrl: "/images/avatar.jpg",//图片，自行修改位置
-        timer: 10000,//弹出时间
-        showConfirmButton: false
+        title: " 欢迎来到我的网站！",
+        text: welcome_text+'\n继续浏览发现更多内容！',//欢迎文本，可自行修改
+        imageUrl: "../img/avatar.png",//图片，自行修改位置
+        timer: 5000,//弹出时间
+        showConfirmButton: true
     });
 }
 $(document).ready(()=>{//若未引用JQuery，请引用
